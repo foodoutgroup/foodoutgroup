7158bdd0829093192354ae7851309b0fc974e594
Mantas <drawgas@drawgas.lt>
2013-11-28 15:00:20 +0200

    Statinio puslapio ispiesimas.

--- src/Food/AppBundle/Controller/StaticController.php
+++ src/Food/AppBundle/Controller/StaticController.php
@@ -2,16 +2,24 @@
 
 namespace Food\AppBundle\Controller;
 
-class StaticController
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class StaticController extends Controller
 {
     public function indexAction($id)
     {
 
-//        $this->
+        $staticPage = $this->get('food.static')->getPage($id);
+
+        if (!$staticPage) {
+            // TODO normalus error page'ai https://basecamp.com/2470154/projects/4420182-skanu-lt-gamyba/todos/72718077-pasidaryti-custom#events_todo_72718077
+            throw new NotFoundHttpException('Sorry not existing!');
+        }
 
         return $this->render(
-            'FoodAppBundle:Static:index.html.twig', array()
-//            array('name' => $name)
+            'FoodAppBundle:Static:index.html.twig',
+            array('staticPage' => $staticPage)
         );
     }
 }
