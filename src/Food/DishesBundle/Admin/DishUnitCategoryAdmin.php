858f96e93d88bdd642f819ccdb69323bf72fa73f
piktas_eziukas <madguru@gmail.com>
2013-12-18 10:03:58 +0200

    Admin prapletimas.

---
+++ src/Food/DishesBundle/Admin/DishUnitCategoryAdmin.php
@@ -0,0 +1,50 @@
+<?php
+namespace Food\DishesBundle\Admin;
+
+use Food\AppBundle\Admin\Admin as FoodAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class DishUnitCategoryAdmin extends FoodAdmin
+{
+    /**
+     * Default Datagrid values
+     *
+     * @var array
+     */
+    protected $datagridValues = array (
+        '_page' => 1, // Display the first page (default = 1)
+        '_sort_order' => 'ASC', // Descendant ordering (default = 'ASC')
+        '_sort_by' => 'id' // name of the ordered field (default = the model id field, if any)
+    );
+
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+
+        //$formMapper->add('unitCategory');
+        //$formMapper->add('place', 'entity', array('class' => 'Food\DishesBundle\Entity\Place'));
+        $formMapper->add('name', null, array('label' => 'admin.dish.unit.category.name'));
+        $formMapper->add('place', null, array('label' => 'admin.dish.unit.category.place'));
+
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('name', null, array('label' => 'admin.dish.unit.category.name'))
+            ->add('place', null, array('label' => 'admin.dish.unit.category.place'))
+        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('name', 'string', array('label' => 'admin.dish.unit.category.name'))
+            ->add('place', 'entity', array('label' => 'admin.dish.unit.category.place'))
+        ;
+    }
+}
