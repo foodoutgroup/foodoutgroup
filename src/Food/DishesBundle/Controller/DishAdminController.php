808d34fdcdd6fbcd45e8171da00a954900b360e4
Mantas <drawgas@drawgas.lt>
2014-03-19 09:27:18 +0200

    Itrauktas Fos JS bundlas ir pajungtas i admin Disho admine maisto kategorijos uzkraunamos pagal place ajaxu

---
+++ src/Food/DishesBundle/Controller/DishAdminController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Food\DishesBundle\Controller;
+
+use Sonata\AdminBundle\Controller\CRUDController as Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class DishAdminController extends Controller
+{
+    public function getCategoryOptionsFromPlaceAction($placeId)
+    {
+        $html = ""; // HTML as response
+        $tag = $this->getDoctrine()
+            ->getRepository('FoodDishesBundle:Place')
+            ->find($placeId);
+
+        $categories = $tag->getCategories();
+
+        foreach($categories as $category){
+            if ($category->getActive())
+            $html .= '<option value="'.$category->getId().'" >'.$category->getName().'</option>';
+        }
+
+        return new Response($html, 200);
+    }
+}
