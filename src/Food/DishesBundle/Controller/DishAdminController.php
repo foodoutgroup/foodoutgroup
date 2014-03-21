d20bb47dd1fefa8b6601330099c0f6722141d0b1
Mantas <drawgas@drawgas.lt>
2014-03-21 16:35:38 +0200

    Patiekalo priedai uzkraunami pagal parinkta restorana

--- src/Food/DishesBundle/Controller/DishAdminController.php
+++ src/Food/DishesBundle/Controller/DishAdminController.php
@@ -24,4 +24,30 @@ class DishAdminController extends Controller
 
         return new Response($html, 200);
     }
+
+    public function getDishOptionsFromPlaceAction($placeId, $uniqueId)
+    {
+        $html = ""; // HTML as response
+        $options = $this->getDoctrine()
+            ->getRepository('FoodDishesBundle:DishOption')
+            ->findBy(array('place' => $placeId));
+
+        $elementNo = 0;
+
+        foreach($options as $option){
+            if (!$option->getHidden()) {
+                $elementNo++;
+
+                $html .=
+                    '<li style="width: 220px; float: left; display: block">'
+                    .'  <label>'.
+                    '       <input type="checkbox" id="'.$uniqueId.'_options_'.$elementNo.'" name="'.$uniqueId.'[options][]" value="'.$option->getId().'">'
+                    .'      <span>'.$option->getName().'</span>'
+                    .'  </label>'
+                    .'</li>';
+            }
+        }
+
+        return new Response($html, 200);
+    }
 }
