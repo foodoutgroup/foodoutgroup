c2f84ec2f98cefd3eb9da5acdfc33beab62f07ce
Mantas <drawgas@drawgas.lt>
2014-05-01 23:16:23 +0300

    #157 - refaktoringo pradzia

--- src/Food/UserBundle/Form/Type/UserAddressFormType.php
+++ src/Food/UserBundle/Form/Type/UserAddressFormType.php
@@ -8,6 +8,15 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class UserAddressFormType extends AbstractType
 {
+    private $availableCities = array();
+
+    function __construct($cities)
+    {
+        $this->setAvailableCities($cities);
+
+        parent::_construct();
+    }
+
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
@@ -24,8 +33,30 @@ class UserAddressFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('city', 'choice', ['label' => 'form.city', 'translation_domain' => 'FOSUserBundle', 'choices' => ['Vilnius', 'Kaunas'], 'required' => true, 'empty_value' => '-', 'attr' => ['class' => 'custom-select']])
+            ->add(
+                'city',
+                'choice',
+                [
+                    'label' => 'form.city',
+                    'translation_domain' => 'FOSUserBundle',
+                    'choices' => $this->availableCities,
+                    'required' => true,
+                    'empty_value' => '-',
+                    'attr' => ['class' => 'custom-select']
+                ]
+            )
             ->add('address', null, ['label' => 'form.address', 'translation_domain' => 'FOSUserBundle', 'required' => false])
         ;
+    }
+
+    /**
+     * @param array $cities
+     * @return $this
+     */
+    public function setAvailableCities($cities)
+    {
+        $this->availableCities = $cities;
+
+        return $this;
     }
 }
