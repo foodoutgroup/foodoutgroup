cff9eb18ac87dc0b3063f57b4cb037637faf8456
piktas_eziukas <madguru@gmail.com>
2014-06-03 19:13:07 +0300

    #281 Restaurant collection

---
+++ src/Food/ApiBundle/Controller/RestaurantsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Food\ApiBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class RestaurantsController extends Controller
+{
+    public function getRestaurantsAction()
+    {
+
+    }
+
+    public function getRestaurantsFilteredAction()
+    {
+
+    }
+
+    public function getRestaurantAction($id)
+    {
+
+    }
+}
