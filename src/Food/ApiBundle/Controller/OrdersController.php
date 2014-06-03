eebc67c614ec418ed707b7cbe443df97ced30427
piktas_eziukas <madguru@gmail.com>
2014-06-03 10:37:32 +0300

    Closes #280

---
+++ src/Food/ApiBundle/Controller/OrdersController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Food\ApiBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class OrdersController extends Controller
+{
+    public function getOrdersAction()
+    {
+
+    }
+
+    public function createOrderAction()
+    {
+
+    }
+
+    public function getOrderDetailsAction($id)
+    {
+
+    }
+
+    public function confirmOrderAction($id)
+    {
+
+    }
+
+    public function getOrderStatusAction($id)
+    {
+
+    }
+}
