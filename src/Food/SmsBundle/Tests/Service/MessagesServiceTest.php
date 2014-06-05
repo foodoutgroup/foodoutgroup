daa5d1d7c9cf2e0b8eaf46eb1599dface1ade2f0
drawgas <drawgas@gmail.com>
2014-06-05 17:16:45 +0300

    #321 - fixo testas :)

--- src/Food/SmsBundle/Tests/Service/MessagesServiceTest.php
+++ src/Food/SmsBundle/Tests/Service/MessagesServiceTest.php
@@ -307,7 +307,58 @@ class MessagesServiceTest extends \PHPUnit_Framework_TestCase {
         $expectedCallValue = array(
             array(
                 'sender' => 'skanu',
-                'text' => 'Pica iskepe',
+                'text' => 'Pica+iskepe',
+                'recipients' => array(
+                    array('gsm' => 37061514333),
+                )
+            )
+        );
+
+        $fakeInfobipResponse = '{"results": [{"status":"0","messageid":"072101113352779063","destination":"385951111111"}]}';
+
+        $expectedParseReturnValue = array(
+            'sent' => true,
+            'status' => 8,
+            'messageid' => 123456,
+            'error' => null,
+            'destination' => '37061514333',
+        );
+
+        $infobipProvider->expects($this->once())
+            ->method('call')
+            ->with($this->equalTo($expectedCallValue))
+            ->will($this->returnValue($fakeInfobipResponse));
+
+        $infobipProvider->expects($this->once())
+            ->method('parseResponse')
+            ->with($this->equalTo($fakeInfobipResponse))
+            ->will($this->returnValue($expectedParseReturnValue));
+
+        $messagesService->sendMessage($message);
+    }
+
+    public function testInfobipSendSpecialChars()
+    {
+        /**
+         * @var \Food\SmsBundle\Service\InfobipProvider $infobipProvider
+         */
+        $infobipProvider = $this->getMock(
+            '\Food\SmsBundle\Service\InfobipProvider',
+            array('call', 'setApiUrl', 'authenticate', 'parseResponse')
+        );
+        $messagesService = new MessagesService($this->container, $infobipProvider);
+
+        $message = new Message();
+        $message->setSender('skanu')
+            ->setRecipient('37061514333')
+            ->setMessage('Pica iskepe. Jei kas negerai, skambinkit +37061514333. Arba ne!')
+            ->setCreatedAt(new \DateTime("now"))
+        ;
+
+        $expectedCallValue = array(
+            array(
+                'sender' => 'skanu',
+                'text' => 'Pica+iskepe.+Jei+kas+negerai%2C+skambinkit+%2B37061514333.+Arba+ne%21',
                 'recipients' => array(
                     array('gsm' => 37061514333),
                 )
