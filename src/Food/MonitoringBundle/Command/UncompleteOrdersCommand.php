207f0039f8c4e504dfa7b0eb14f0a20734437e14
drawgas <drawgas@gmail.com>
2014-06-17 10:50:46 +0300

    #323 - neuzdarytu pastaruju 2 dienu pirkimu monitoringas

---
+++ src/Food/MonitoringBundle/Command/UncompleteOrdersCommand.php
@@ -0,0 +1,76 @@
+<?php
+namespace Food\MonitoringBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class UncompleteOrdersCommand
+ * @package Food\MonitoringBundle\Command
+ */
+class UncompleteOrdersCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('monitoring:order:uncomplete')
+            ->setDescription('Check there are uncomplete orders for yesterday')
+            ->addOption(
+                'debug',
+                null,
+                InputOption::VALUE_NONE,
+                'If set, debug information will be logged'
+            )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $monitoringService = $this->getContainer()->get('food.monitoring');
+
+        $from = new \DateTime(
+            date("Y-m-d 00:00:01", strtotime("-3 day"))
+        );
+        $to = new \DateTime(
+            date("Y-m-d 23:59:59", strtotime("-1 day"))
+        );
+        $critical = false;
+
+        try {
+            $unfinishedOrders = $monitoringService->getUnfinishedOrdersForRange($from, $to);
+            $ordersCount = count($unfinishedOrders);
+
+            if ($ordersCount > 0) {
+                $orderIds = array();
+
+                foreach($unfinishedOrders as $order) {
+                    $orderIds[] = $order->getId();
+                }
+
+                $text = sprintf(
+                    '<error>ERROR: %d uncomplete orders! Ids: %s</error>',
+                    $ordersCount,
+                    implode(', ', $orderIds)
+                );
+                $critical = true;
+            } else {
+                $text = '<info>OK: all orders are completed successfuly</info>';
+            }
+        } catch (\Exception $e) {
+            $text = '<error>ERROR: Error in unfinished orders check: '.$e->getMessage().'</error>';
+            $output->writeln($text);
+
+            throw $e;
+        }
+
+        $output->writeln($text);
+
+        if ($critical) {
+            return 2;
+        }
+
+        return 0;
+    }
+}
---
+++ src/Food/MonitoringBundle/Tests/Command/UncompleteOrdersCommandTest.php
@@ -0,0 +1,226 @@
+<?php
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class UncompleteOrdersCommandTest extends \PHPUnit_Framework_TestCase
+{
+    public function testNoUncompleteOrders()
+    {
+        $expectedReturn = 0;
+
+        $orders = array();
+        $fromDate = new \DateTime(
+            date("Y-m-d 00:00:01", strtotime("-3 day"))
+        );
+        $toDate = new \DateTime(
+            date("Y-m-d 23:59:59", strtotime("-1 day"))
+        );
+
+        $container = $this->getMock(
+            'Symfony\Component\DependencyInjection\Container',
+            array('getParameter', 'get')
+        );
+        $monitoringService = $this->getMockBuilder('\Food\MonitoringBundle\Service\MonitoringService')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $application = new Application();
+        $application->add(new \Food\MonitoringBundle\Command\UncompleteOrdersCommand());
+
+        /**
+         * @var SendCommand $command
+         */
+        $command = $application->find('monitoring:order:uncomplete');
+        $command->setContainer($container);
+
+        $container->expects($this->at(0))
+            ->method('get')
+            ->with('food.monitoring')
+            ->will($this->returnValue($monitoringService));
+
+        $monitoringService->expects($this->once())
+            ->method('getUnfinishedOrdersForRange')
+            ->with($fromDate, $toDate)
+            ->will($this->returnValue($orders));
+
+
+        $commandTester = new CommandTester($command);
+        $gotReturn = $commandTester->execute(
+            array('command' => $command->getName())
+        );
+
+        $this->assertRegExp('/all orders are completed successfuly/', $commandTester->getDisplay());
+        $this->assertEquals($expectedReturn, $gotReturn);
+    }
+
+    public function testOneUncompleteOrder()
+    {
+        $expectedReturn = 2;
+
+        $order = $this->getMockBuilder('Food\OrderBundle\Entity\Order')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $orders = array($order);
+
+        $fromDate = new \DateTime(
+            date("Y-m-d 00:00:01", strtotime("-3 day"))
+        );
+        $toDate = new \DateTime(
+            date("Y-m-d 23:59:59", strtotime("-1 day"))
+        );
+
+        $container = $this->getMock(
+            'Symfony\Component\DependencyInjection\Container',
+            array('getParameter', 'get')
+        );
+        $monitoringService = $this->getMockBuilder('\Food\MonitoringBundle\Service\MonitoringService')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $application = new Application();
+        $application->add(new \Food\MonitoringBundle\Command\UncompleteOrdersCommand());
+
+        /**
+         * @var SendCommand $command
+         */
+        $command = $application->find('monitoring:order:uncomplete');
+        $command->setContainer($container);
+
+        $container->expects($this->at(0))
+            ->method('get')
+            ->with('food.monitoring')
+            ->will($this->returnValue($monitoringService));
+
+        $monitoringService->expects($this->once())
+            ->method('getUnfinishedOrdersForRange')
+            ->with($fromDate, $toDate)
+            ->will($this->returnValue($orders));
+
+        $order->expects($this->once())
+            ->method('getId')
+            ->will($this->returnValue(154));
+
+        $commandTester = new CommandTester($command);
+        $gotReturn = $commandTester->execute(
+            array('command' => $command->getName())
+        );
+
+        $this->assertRegExp('/1 uncomplete orders! Ids: 154/', $commandTester->getDisplay());
+        $this->assertEquals($expectedReturn, $gotReturn);
+    }
+
+    public function testThreeUncompleteOrder()
+    {
+        $expectedReturn = 2;
+
+        $order = $this->getMockBuilder('Food\OrderBundle\Entity\Order')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $orders = array($order, $order, $order);
+
+        $fromDate = new \DateTime(
+            date("Y-m-d 00:00:01", strtotime("-3 day"))
+        );
+        $toDate = new \DateTime(
+            date("Y-m-d 23:59:59", strtotime("-1 day"))
+        );
+
+        $container = $this->getMock(
+            'Symfony\Component\DependencyInjection\Container',
+            array('getParameter', 'get')
+        );
+        $monitoringService = $this->getMockBuilder('\Food\MonitoringBundle\Service\MonitoringService')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $application = new Application();
+        $application->add(new \Food\MonitoringBundle\Command\UncompleteOrdersCommand());
+
+        /**
+         * @var SendCommand $command
+         */
+        $command = $application->find('monitoring:order:uncomplete');
+        $command->setContainer($container);
+
+        $container->expects($this->at(0))
+            ->method('get')
+            ->with('food.monitoring')
+            ->will($this->returnValue($monitoringService));
+
+        $monitoringService->expects($this->once())
+            ->method('getUnfinishedOrdersForRange')
+            ->with($fromDate, $toDate)
+            ->will($this->returnValue($orders));
+
+        $order->expects($this->at(0))
+            ->method('getId')
+            ->will($this->returnValue(128));
+
+        $order->expects($this->at(1))
+            ->method('getId')
+            ->will($this->returnValue(165));
+
+        $order->expects($this->at(2))
+            ->method('getId')
+            ->will($this->returnValue(187));
+
+        $commandTester = new CommandTester($command);
+        $gotReturn = $commandTester->execute(
+            array('command' => $command->getName())
+        );
+
+        $this->assertRegExp('/3 uncomplete orders! Ids: 128, 165, 187/', $commandTester->getDisplay());
+        $this->assertEquals($expectedReturn, $gotReturn);
+    }
+
+    /**
+     * @expectedException \Exception
+     * @expectedExceptionMessage I failed
+     */
+    public function testExceptionHappened()
+    {
+        $expectedReturn = 2;
+
+        $fromDate = new \DateTime(
+            date("Y-m-d 00:00:01", strtotime("-3 day"))
+        );
+        $toDate = new \DateTime(
+            date("Y-m-d 23:59:59", strtotime("-1 day"))
+        );
+
+        $container = $this->getMock(
+            'Symfony\Component\DependencyInjection\Container',
+            array('getParameter', 'get')
+        );
+        $monitoringService = $this->getMockBuilder('\Food\MonitoringBundle\Service\MonitoringService')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $application = new Application();
+        $application->add(new \Food\MonitoringBundle\Command\UncompleteOrdersCommand());
+
+        /**
+         * @var SendCommand $command
+         */
+        $command = $application->find('monitoring:order:uncomplete');
+        $command->setContainer($container);
+
+        $container->expects($this->at(0))
+            ->method('get')
+            ->with('food.monitoring')
+            ->will($this->returnValue($monitoringService));
+
+        $monitoringService->expects($this->once())
+            ->method('getUnfinishedOrdersForRange')
+            ->with($fromDate, $toDate)
+            ->will($this->throwException(new \Exception('I failed')));
+
+        $commandTester = new CommandTester($command);
+        $gotReturn = $commandTester->execute(
+            array('command' => $command->getName())
+        );
+
+        $this->assertRegExp('/Error in unfinished orders check: I failed/', $commandTester->getDisplay());
+        $this->assertEquals($expectedReturn, $gotReturn);
+    }
+}
