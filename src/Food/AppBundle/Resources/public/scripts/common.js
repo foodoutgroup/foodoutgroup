aeefcbac91675ff4e89968056af8e77cc48634c4
drawgas <drawgas@gmail.com>
2014-07-04 16:45:50 +0300

    #385 - Vynoguru reik, kad patiekale viskas popup is naujos eilutes butu..

--- src/Food/AppBundle/Resources/public/scripts/common.js
+++ src/Food/AppBundle/Resources/public/scripts/common.js
@@ -13,3 +13,20 @@ function alertBox(title_msg, content_msg, okText) {
         }
     });
 };
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i].trim();
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
+};
+
+function setCookie(cname, cvalue, exmins) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exmins*60*1000));
+    var expires = "expires="+d.toGMTString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+};
