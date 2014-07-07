b003af5bbbbe641966a081e2f349b4ed4cea0e23
drawgas <drawgas@gmail.com>
2014-07-07 14:36:35 +0300

    #387 - priminimo kronas negali ideti url i nuoroda, nes jis to neturi - juk tai komanda, dude :P

--- src/Food/OrderBundle/Service/OrderService.php
+++ src/Food/OrderBundle/Service/OrderService.php
@@ -1521,13 +1521,18 @@ class OrderService extends ContainerAware
         $domain = $this->container->getParameter('domain');
 
         // Inform restourant about new order
-        $orderConfirmRoute = $this->container->get('router')
-            ->generate('ordermobile', array('hash' => $order->getOrderHash()), true);
 
         if ($isReminder) {
+            $orderConfirmRoute = 'http://'.$domain
+                .$this->container->get('router')
+                    ->generate('ordermobile', array('hash' => $order->getOrderHash()));
+
             $orderSmsTextTranslation = $translator->trans('general.sms.order_reminder');
             $orderTextTranslation = $translator->trans('general.email.order_reminder');
         } else {
+            $orderConfirmRoute = $this->container->get('router')
+                ->generate('ordermobile', array('hash' => $order->getOrderHash()), true);
+
             $orderSmsTextTranslation = $translator->trans('general.sms.new_order');
             $orderTextTranslation = $translator->trans('general.email.new_order');
         }
