83da93e27f9521c7add0388422753d6b1113f2ab
drawgas <drawgas@gmail.com>
2014-07-09 16:57:48 +0300

    #293 - gatviu ac. truksta importo

---
+++ src/Food/ApiBundle/Controller/AddressController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Food\ApiBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class AddressController extends Controller
+{
+    public function findStreetAction(Request $request)
+    {
+        $queryPart = $request->get('query');
+        $response = array();
+
+        if (!empty($queryPart)) {
+            $streets = $this->get('food.logistics')->findStreet($queryPart, 5);
+
+            if (!empty($streets)) {
+                foreach ($streets as $street) {
+                    $response[] = array(
+                        'street' => $street->getStreet(),
+                        'city' => $street->getCity(),
+                    );
+                }
+            }
+        }
+
+        return new JsonResponse($response);
+    }
+}
