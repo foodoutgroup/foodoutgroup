ed0a457d6a95f6b08162e69292e00459f3663e92
pirminis <pirminis@gmail.com>
2014-10-10 14:00:06 +0300

    .

--- src/Food/OrderBundle/Controller/Decorators/Seb/ReturnDecorator.php
+++ src/Food/OrderBundle/Controller/Decorators/Seb/ReturnDecorator.php
@@ -46,6 +46,10 @@ trait ReturnDecorator
         } catch (\Exception $e) {
         }
 
+        var_dump($orderId);
+        var_dump($service);
+        var_dump($verified);
+
         if ($verified) {
             if (SebService::WAITING_SERVICE == $service) {
                 // template
