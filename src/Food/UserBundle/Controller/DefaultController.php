8cba6c3a0ab283296835f415b1a7a9c13bf21212
pirminis <pirminis@gmail.com>
2014-11-24 03:18:59 +0200

    Refactored user registration.
    Now it is executed in accord to FOS user bundle [new] rules. So no more
    “already submitted” bug, also fixes other bugs that were critical.

--- src/Food/UserBundle/Controller/DefaultController.php
+++ src/Food/UserBundle/Controller/DefaultController.php
@@ -12,11 +12,6 @@ use Symfony\Component\Security\Core\SecurityContext;
 use Symfony\Component\Form\FormError;
 use Symfony\Component\Form\Form;
 use Doctrine\Common\Collections\ArrayCollection;
-// TODO Jonai cia del naujos FOS versijos uzkomentavau
-//use FOS\UserBundle\Event\GetResponseUserEvent;
-//use FOS\UserBundle\FOSUserEvents;
-//use FOS\UserBundle\Event\FormEvent;
-//use FOS\UserBundle\Event\FilterUserResponseEvent;
 use Food\UserBundle\Form\Type\ProfileFormType;
 use Food\UserBundle\Form\Type\ProfileMegaFormType;
 use Food\UserBundle\Form\Type\UserAddressFormType;
@@ -34,81 +29,23 @@ class DefaultController extends Controller
     public function registerCreateAction(Request $request)
     {
         $form = $this->container->get('fos_user.registration.form');
-        $userManager = $this->container->get('fos_user.user_manager');
-        // TODO Jonai cia del naujos FOS versijos uzkomentavau
-//        $dispatcher = $this->container->get('event_dispatcher');
-        $translator = $this->container->get('translator');
-        $notifications = $this->container->get('food.app.utils.notifications');
+        // we will be using extended version of fos registration form handler
+        // $formHandler = $this->container->get('fos_user.registration.form.handler');
+        $formHandler = $this->container->get('food_user.registration.form.handler');
 
-        $user = $userManager->createUser();
-        $user->setUsername(uniqid('', true));
-        $user->setEnabled(true);
+        // we have no confirmation email, hence this parameter is always false
+        // $confirmationEnabled = $this->container->getParameter('fos_user.registration.confirmation.enabled');
+        $confirmationEnabled = false;
 
-        // TODO Jonai cia del naujos FOS versijos uzkomentavau
-//        $event = new GetResponseUserEvent($user, $request);
-//        $dispatcher->dispatch(FOSUserEvents::REGISTRATION_INITIALIZE, $event);
+        // check if user exists
+        $email = $request->request->get('fos_user_registration_form[email]', '', true);
+        $existingUser = $this->userExists($email);
 
-//        if (null !== $event->getResponse()) {
-//            return $event->getResponse();
-//        }
+        $formHandler->setUser($existingUser);
+        $process = $formHandler->process($confirmationEnabled);
 
-//        $form = $formFactory->createForm();
-        $form->setData($user);
-        $form->bind($request);
-
-        $existingUser = $this->userExists($form->get('email')->getData());
-
-        // rebind if user exists
-        if ($existingUser) {
-            $user = $existingUser;
-
-//            $form = $formFactory->createForm();
-            $form->setData($user);
-            $form->bind($request);
-        }
-
-        if ($form->isValid() && (!$existingUser || ($existingUser && !$existingUser->getFullyRegistered()))) {
-            // TODO Jonai cia del naujos FOS versijos uzkomentavau
-//            $event = new FormEvent($form, $request);
-//            $dispatcher->dispatch(FOSUserEvents::REGISTRATION_SUCCESS, $event);
-
-            // manually set these flags. ->setEnabled is especially important for password update if user exists.
-            $user->setEnabled(true);
-            $user->setFullyRegistered(true);
-
-            // finally update user
-            $userManager->updateUser($user);
-
-            // set noty notification for successful user registration
-            $notifications->setSuccessMessage($translator->trans('general.successful_user_registration'));
-
-            $d = $request->get('fos_user_registration_form');
-
-            $this->_notifyNewUser($user, $d['plainPassword']['first']);
-
-            // TODO Jonai cia del naujos FOS versijos uzkomentavau
-//            if (null === $response = $event->getResponse()) {
-//                $url = $this->container->get('router')->generate('food_lang_homepage');
-//                $response = new Response('');
-//            }
-
-            $oldSid = $request->getSession()->getId();
-            // TODO Jonai cia del naujos FOS versijos uzkomentavau
-//            $dispatcher->dispatch(FOSUserEvents::REGISTRATION_COMPLETED, new FilterUserResponseEvent($user, $request, $response));
-            $newSid = $request->getSession()->getId();
-
-            /**
-             * @TODO - WATAFAK. Tik taip uzgesinom gaisra. Need to fix normaly :)
-             */
-            $this->get('food.cart')->migrateCartBetweenSessionIds($oldSid, $newSid);
-
-            // TODO Jonai cia del naujos FOS versijos uzkomentavau - cia hack responsas :(
-            $response = new Response('');
-            return $response;
-        }
-
-        if ($existingUser) {
-            $form->get('email')->addError(new FormError('This user is already registered.'));
+        if ($process) {
+            return new Response('');
         }
 
         return $this->render(
@@ -119,23 +56,6 @@ class DefaultController extends Controller
                 'submitted' => $form->isSubmitted()
             ]
         );
-    }
-
-    /**
-     * @param User $user
-     * @param $pass
-     */
-    private function _notifyNewUser($user, $pass)
-    {
-        $ml = $this->get('food.mailer');
-
-        $variables = array(
-            'username' => $user->getUsername(),
-            'password' => $pass,
-            'login_url' => $this->generateUrl('food_lang_homepage', array(), true),
-        );
-
-        $ml->setVariables( $variables )->setRecipient( $user->getEmail(), $user->getEmail())->setId( 30009253 )->send();
     }
 
     /**
@@ -304,7 +224,7 @@ class DefaultController extends Controller
                 ->findByEmail($email)
         );
 
-        return $existingUser->first();
+        return \Maybe($existingUser)->first()->val(null, true);
     }
 
     private function user()
---
+++ src/Food/UserBundle/Service/RegistrationFormHandler.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Food\UserBundle\Service;
+
+use FOS\UserBundle\Form\Handler\RegistrationFormHandler as OriginalHandler;
+use FOS\UserBundle\Model\UserManagerInterface;
+use FOS\UserBundle\Model\UserInterface;
+use FOS\UserBundle\Mailer\MailerInterface;
+use FOS\UserBundle\Util\TokenGeneratorInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class RegistrationFormHandler extends OriginalHandler
+{
+    protected $user;
+    protected $translator;
+    protected $notifications;
+    protected $foodMailer;
+    protected $router;
+    protected $cart;
+
+    public function __construct(FormInterface $form,
+                                Request $request,
+                                UserManagerInterface $userManager,
+                                MailerInterface $mailer,
+                                TokenGeneratorInterface $tokenGenerator,
+                                $translator,
+                                $notifications,
+                                $foodMailer,
+                                $router,
+                                $cart)
+    {
+        parent::__construct($form,
+                            $request,
+                            $userManager,
+                            $mailer,
+                            $tokenGenerator);
+
+        $this->translator = $translator;
+        $this->notifications = $notifications;
+        $this->foodMailer = $foodMailer;
+        $this->router = $router;
+        $this->cart = $cart;
+    }
+
+    /**
+     * @param boolean $confirmation
+     */
+    public function process($confirmation = false)
+    {
+        $existingUser = $this->getUser();
+        $fullyRegistered = !!\Maybe($existingUser)->getFullyRegistered()
+                                                  ->val(false);
+
+        $user = $this->createUser();
+        
+        if ($existingUser && !$fullyRegistered) {
+            $user = $existingUser;
+        }
+
+        $user->setUsername($user->getEmail());
+        $user->setEnabled(true);
+
+        $this->form->setData($user);
+
+        if ('POST' === $this->request->getMethod()) {
+            $this->form->bind($this->request);
+
+            if ($this->form->isValid()) {
+                $this->onSuccess($user, $confirmation);
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @param boolean $confirmation
+     */
+    protected function onSuccess(UserInterface $user, $confirmation)
+    {
+        $user->setEnabled(true);
+        $user->setFullyRegistered(true);
+
+        // set noty notification for successful user registration
+        $this->notifications->setSuccessMessage(
+            $this->translator->trans('general.successful_user_registration'));
+
+        $d = $this->request->get('fos_user_registration_form');
+
+        $this->_notifyNewUser($user, $d['plainPassword']['first']);
+
+        // mysterious problem solver
+        $oldSid = $this->request->getSession()->getId();
+        $newSid = $this->request->getSession()->getId();
+        $this->cart->migrateCartBetweenSessionIds($oldSid, $newSid);
+
+        $this->userManager->updateUser($user);
+    }
+
+    public function setUser(UserInterface $user = null)
+    {
+        $this->user = $user;
+    }
+
+    protected function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param User $user
+     * @param $pass
+     */
+    protected function _notifyNewUser($user, $pass)
+    {
+        $variables = [
+            'username' => $user->getUsername(),
+            'password' => $pass,
+            'login_url' => $this->router
+                                ->generate('food_lang_homepage', [], true)
+        ];
+
+        $this->foodMailer
+             ->setVariables($variables)
+             ->setRecipient($user->getEmail(),
+                            $user->getEmail())
+             ->setId(30009253)
+             ->send();
+    }
+}
