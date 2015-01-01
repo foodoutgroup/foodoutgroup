f55b374e812f6292853b825a2776cae2b54ba7e1
piktas_eziukas <madguru@gmail.com>
2015-01-01 12:57:57 +0200

    Crazy testing.

--- src/Food/AppBundle/Controller/TestController.php
+++ src/Food/AppBundle/Controller/TestController.php
@@ -343,10 +343,12 @@ class TestController extends Controller
 
     public function listOrdersAction() {
         $ns = $this->get('food.nav');
-        $query = "SELECT TOP 20 * FROM [skamb_centras].[dbo].[Čilija Skambučių Centras$Web ORDER Header] ORDER BY [Order No_] DESC";
+        $query = 'SELECT TOP 20 * FROM [skamb_centras].[dbo].[Čilija Skambučių Centras$Web ORDER Header] ORDER BY [Order No_] DESC';
         $rez = $ns->initSqlConn()->query($query);
         echo "<pre>";
-        var_dump($ns->initSqlConn()->fetchArray($rez));
+        while ($rowRez = $this->get('food.mssql')->fetchArray($rez)) {
+            var_dump($rowRez);
+        }
         die();
     }
 }
