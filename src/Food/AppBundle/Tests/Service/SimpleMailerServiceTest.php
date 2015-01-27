25867ba2e4cd18e5f9d3355662a88228ed1a070e
pirminis <pirminis@gmail.com>
2015-01-27 12:31:55 +0200

    Made simple mailer test a little bit more robust and clear.

--- src/Food/AppBundle/Tests/Service/SimpleMailerServiceTest.php
+++ src/Food/AppBundle/Tests/Service/SimpleMailerServiceTest.php
@@ -11,47 +11,6 @@ class SimpleMailerServiceTest extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->simpleMailer = new SimpleMailerService();
-
-        $mailerMock = $this->getMockBuilder('\StdClass')
-                           ->setMethods(['createMessage',
-                                         'send',
-                                         'getTransport',
-                                         'getSpool',
-                                         'flushQueue'])
-                           ->getMock();
-
-        $transportMock = $this->getMockBuilder('\StdClass')
-                              ->getMock();
-
-        $messageMock = $this->getMockBuilder('\StdClass')
-                            ->setMethods(['setFrom',
-                                          'setTo',
-                                          'setSubject',
-                                          'setBody'])
-                            ->getMock();
-
-        $mailerMock->expects($this->any())
-                   ->method('createMessage')
-                   ->willReturn($messageMock);
-
-        $mailerMock->expects($this->any())
-                   ->method('send')
-                   ->willReturn(null);
-
-        $mailerMock->expects($this->any())
-                   ->method('flushQueue')
-                   ->willReturn(1);
-
-        $mailerMock->expects($this->any())
-                   ->method($this->anything())
-                   ->willReturn($mailerMock);
-
-        $messageMock->expects($this->any())
-                    ->method($this->anything())
-                    ->willReturn($messageMock);
-
-        $this->simpleMailer->setMailer($mailerMock);
-        $this->simpleMailer->setTransport($transportMock);
     }
 
     public function testSetGetMailer()
@@ -76,6 +35,9 @@ class SimpleMailerServiceTest extends \PHPUnit_Framework_TestCase
 
     public function testSend()
     {
+        $this->simpleMailer->setMailer($this->getMailerMock());
+        $this->simpleMailer->setTransport($this->getTransportMock());
+
         $from = 'some@email.com';
         $to = 'some2@email.com';
         $subject = 'subject line';
@@ -84,5 +46,63 @@ class SimpleMailerServiceTest extends \PHPUnit_Framework_TestCase
         $result = $this->simpleMailer->send($from, $to, $subject, $body);
 
         $this->assertSame(1, $result);
+    }
+
+    public function getMailerMock()
+    {
+        $mailerMock = $this->getMockBuilder('\StdClass')
+                           ->setMethods(['createMessage',
+                                         'send',
+                                         'getTransport',
+                                         'getSpool',
+                                         'flushQueue'])
+                           ->getMock();
+
+        // we have three main points in send() method:
+        // 1. createMessage()
+        // 2. send()
+        // 3. flushQueue()
+        $mailerMock->expects($this->any())
+                   ->method('createMessage')
+                   ->willReturn($this->getMessageMock());
+
+        $mailerMock->expects($this->any())
+                   ->method('send')
+                   ->willReturn(null);
+
+        $mailerMock->expects($this->any())
+                   ->method('flushQueue')
+                   ->willReturn(1);
+
+        // the rest is useless
+        $mailerMock->expects($this->any())
+                   ->method($this->anything())
+                   ->willReturn($mailerMock);
+
+        return $mailerMock;
+    }
+
+    public function getTransportMock()
+    {
+        $transportMock = $this->getMockBuilder('\StdClass')
+                              ->getMock();
+
+        return $transportMock;
+    }
+
+    public function getMessageMock()
+    {
+        $messageMock = $this->getMockBuilder('\StdClass')
+                            ->setMethods(['setFrom',
+                                          'setTo',
+                                          'setSubject',
+                                          'setBody'])
+                            ->getMock();
+
+        $messageMock->expects($this->any())
+                    ->method($this->anything())
+                    ->willReturn($messageMock);
+
+        return $messageMock;
     }
 }
