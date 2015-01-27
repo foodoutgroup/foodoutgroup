ae4bcd0f677652531abb5f38728cb8ff1228baa8
pirminis <pirminis@gmail.com>
2015-01-27 10:28:49 +0200

    Added simple swiftmail wrapper.

---
+++ src/Food/AppBundle/Service/SimpleMailerService.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Food\AppBundle\Service;
+
+class SimpleMailerService
+{
+    protected $mailer;
+    protected $transport;
+
+    public function send($from, $to, $subject, $body)
+    {
+        $message = $this->getMailer()->createMessage();
+        $message->setFrom($from)
+                ->setTo($to)
+                ->setSubject($subject)
+                ->setBody($body);
+
+        $this->getMailer()->send($message);
+        $success = $this
+            ->getMailer()
+            ->getTransport()
+            ->getSpool()
+            ->flushQueue($this->getTransport());
+
+        return $success;
+    }
+
+    public function setMailer($service)
+    {
+        $this->mailer = $service;
+        return $this;
+    }
+
+    public function getMailer()
+    {
+        return $this->mailer;
+    }
+
+    public function setTransport($service)
+    {
+        $this->transport = $service;
+        return $this;
+    }
+
+    public function getTransport()
+    {
+        return $this->transport;
+    }
+}
