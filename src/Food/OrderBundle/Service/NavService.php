2b5c44890e86996c86bf29a024377f0ac3358f46
piktas_eziukas <madguru@gmail.com>
2016-03-29 13:03:17 +0300

    Little fix - time post

--- src/Food/OrderBundle/Service/NavService.php
+++ src/Food/OrderBundle/Service/NavService.php
@@ -375,7 +375,7 @@ class NavService extends ContainerAware
         $orderDate = $order->getOrderDate();
         $orderDate->add(new \DateInterval('P0DT0H'));
         $deliveryDate = $order->getDeliveryTime();
-        $deliveryDate->sub(new \DateInterval('P0DT2H'));
+        $deliveryDate->sub(new \DateInterval('P0DT3H'));
         /**
          * This thing is when we move to daytime saving mode.
          * @todo put to the CONFIG or make AUTO
