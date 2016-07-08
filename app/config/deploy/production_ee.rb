e6fdb5ff6c22112223edec4c37b1672c0c81805c
Mantas <drawgas@drawgas.lt>
2016-07-08 13:50:35 +0300

    Estonia params

---
+++ app/config/deploy/production_ee.rb
@@ -0,0 +1,21 @@
+set :domain,      "foodout.lt"
+set :deploy_to,   "/home/estonia.foodout.lt/"
+
+set :scm,         :git
+set :model_manager, "doctrine"
+
+set :user, "skanu"
+set :password, "veM6hee0"
+
+set :symfony_env_prod, "prod"
+ssh_options[:keys] = ["C:\Users\drawgas\.ssh\id_rsa"]
+
+role :web,        domain                         # Your HTTP server, Apache/etc
+role :app,        domain                         # This may be the same as your `Web` server
+role :db,         domain, :primary => true       # This is where Symfony2 migrations will run
+
+# send notification to skype
+#set :skype_topic, "skanu.lt"
+
+# parameters file
+set :parameters_file, "production_ee.yml.dist"
