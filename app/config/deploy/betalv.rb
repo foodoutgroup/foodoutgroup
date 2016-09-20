c2f418e788a5b7c2761401d1f9f5f7809e4c1ad2
Karolis M <karolis.m@foodout.lt>
2016-09-20 17:06:41 +0300

    deploy params

---
+++ app/config/deploy/betalv.rb
@@ -0,0 +1,24 @@
+set :domain,      "d.foodout.lt"
+set :deploy_to,   "/srv/vhosts/beta.foodout.lv/"
+
+set :scm,         :git
+set :model_manager, "doctrine"
+
+set :user, "foodoutlv"
+set :password, "RjT8EThq"
+
+set :symfony_env_prod, "prod"
+set :clear_controllers, false
+ssh_options[:keys] = ["C:\Users\drawgas\.ssh\id_rsa"]
+
+role :web,        domain                         # Your HTTP server, Apache/etc
+role :app,        domain                         # This may be the same as your `Web` server
+role :db,         domain, :primary => true       # This is where Symfony2 migrations will run
+
+# send notification to skype
+#set :skype_topic, "skanu.lt"
+
+# parameters file
+set :parameters_file, "production_lv.yml.dist"
+set :kpi_file, "kpi_production_lv.yml.dist"
+set :robots_file, "robots.prod.txt.dist"
