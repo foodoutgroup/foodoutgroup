812af18d798574b89691ec772a6930891a16f3c7
Aleksas <aseksas@gmail.com>
2017-01-31 10:12:04 +0200

    city

---
+++ src/Api/V2Bundle/Controller/LocationController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Api\V2Bundle\Controller;
+
+use Api\BaseBundle\Exceptions\ApiException;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class LocationController extends Controller
+{
+    /**
+     * karolis nesake kaip daryt :D padariau kaip greiciau :D
+     * @param $providerHash
+     * @return JsonResponse
+     */
+    public function getCityAction($providerHash)
+    {
+        $response = ['success' => false];
+
+        switch ($providerHash) {
+            case "3MezrzQPc8gCdXDtpsQETj1qD57iISrd53xMR01UfpIYZU0hxgXioN2QY3GBZo8P": // todo: kazkaip pakeisti cia special for cili
+
+                $cityCollection = [];
+                $placeGroupIDs = [63,85, 105,142,333,302,160];
+                $cityConfigCollection = $this->container->getParameter('available_cities');
+
+                foreach ($cityConfigCollection as $city) {
+
+                    $repository = $this->getDoctrine()
+                        ->getRepository('FoodDishesBundle:PlacePoint');
+
+                    $qb = $repository->createQueryBuilder('pp')
+                        ->innerJoin('FoodDishesBundle:Place', 'p', 'WITH','p.id = pp.place')
+                        ->where('pp.city = :city')
+                        ->andWhere('p.id IN (:ids)')
+                        ->andWhere('p.apiHash IS NOT NULL');
+
+                    $query = $qb->getQuery()->setParameters(['city' => $city, 'ids' => implode(",",$placeGroupIDs)]);
+
+                    $result = $query->execute();
+                    $place = (count($result) >= 1 ? $result[0]->getPlace() : null);
+
+                    if($place != null && $place->getActive()) {
+                        $cityCollection[] = [
+                            'city' => $city,
+                            'hash' => $place->getApiHash(),
+                        ];
+                    }
+                }
+
+                $response['success'] = true;
+                $response['collection'] = $cityCollection;
+
+                break;
+            default:
+                $response['message'] = 'Provider not found';
+                break;
+        }
+        return new JsonResponse($response);
+    }
+}
