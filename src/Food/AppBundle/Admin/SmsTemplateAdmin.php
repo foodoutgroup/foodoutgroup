fdf7d07da1e7b2f25a5216a4af03e9a5a1f2c7e3
Aleksas <aseksas@gmail.com>
2017-04-24 15:57:40 +0300

    sms template window

---
+++ src/Food/AppBundle/Admin/SmsTemplateAdmin.php
@@ -0,0 +1,47 @@
+<?php namespace Food\AppBundle\Admin;
+
+use Food\AppBundle\Admin\Admin as FoodAdmin;
+use Food\OrderBundle\Service\OrderService;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
+
+class SmsTemplateAdmin extends FoodAdmin
+{
+    function configureListFields(ListMapper $list)
+    {
+        $list->add('text', null, ['label' => 'admin.sms.text'])
+            ->add('status', 'text', ['label' => 'admin.sms.text'])
+            ->add('preorder', 'boolean', ['label' => 'admin.sms.preorder'])
+            ->add('type', 'text', ['label' => 'admin.sms.type'])
+            ->add('source', 'boolean', ['label' => 'admin.sms.source'])
+            ->add('_action', 'actions', ['actions' => ['edit' => [], 'delete' => [],], 'label' => 'admin.actions']);
+    }
+
+    function configureFormFields(FormMapper $form)
+    {
+        $sourceCollection = ['NAV', 'Apiv2'];
+        $typeCollection = ['pickup', 'order'];
+        $orderStatusCollection = [];
+        foreach (OrderService::getOrderStatuses() as $os) {
+            $orderStatusCollection[$os] = $os;
+        }
+
+        $templateHelp = $this->getContainer()->get('templating')->render('@FoodApp/Admin/Custom/sms_template_help.html.twig', []);
+
+
+        $form->add('translations', 'a2lix_translations_gedmo', [
+                'translatable_class' => 'Food\AppBundle\Entity\SmsTemplate',
+                'cascade_validation' => true,
+            'fields' => [
+                'text' => ['required' => true, 'attr' => ['help' => $templateHelp]]
+            ]
+        ])
+        ->add('status', 'choice', ['label' => 'admin.sms.status', 'choices' => $orderStatusCollection])
+        ->add('preorder', 'boolean', ['label' => 'admin.sms.preorder'])
+        ->add('type', 'boolean', ['label' => 'admin.sms.type', 'choices' => $typeCollection])
+        ->add('source', 'choice', ['label' => 'admin.sms.source', 'choices' => $sourceCollection])
+        ->add('active', 'boolean');
+    }
+
+}
