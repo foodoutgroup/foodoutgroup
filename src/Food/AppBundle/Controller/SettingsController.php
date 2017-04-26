4ffe0ef4382f3908c1b94ab2f4c3002b79e02ae8
antanas <antanas@foodout.lt>
2017-04-26 12:40:01 +0300

    settings refac

--- src/Food/AppBundle/Controller/SettingsController.php
+++ src/Food/AppBundle/Controller/SettingsController.php
@@ -1,13 +1,15 @@
 <?php
 
-namespace Food\AppBundle\Controller\Admin;
+namespace Food\AppBundle\Controller;
 
 use Sonata\AdminBundle\Controller\CoreController;
 use Sonata\AdminBundle\Controller\CRUDController;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\HttpFoundation\Request;
 
-class SettingsController extends CoreController
+
+use Sonata\AdminBundle\Controller\CRUDController as Controller;
+class SettingsController extends Controller
 {
 
     private $keywordMapCollection = [
@@ -38,8 +40,10 @@ class SettingsController extends CoreController
         'placepoint_prepare_times',
     ];
 
-    public function indexAction(Request $request)
+    public function listAction( )
     {
+
+        $request = $this->getRequest();
         $paramService = $this->get('food.app.utils.misc');
         $session = $this->get('session');
         $data = [];
@@ -63,7 +67,6 @@ class SettingsController extends CoreController
 
             }
             $session->getFlashBag()->add('success', 'Settings was saved successfully');
-            return $this->redirect($this->generateUrl('food_admin_settings'));
         }
 
         return $this->render('@FoodApp/Admin/Settings/index.html.twig', array(
@@ -77,8 +80,6 @@ class SettingsController extends CoreController
 
     private function formFields(FormBuilderInterface &$form)
     {
-
-
         $static = $this->getDoctrine()->getRepository('FoodAppBundle:StaticContent');
         $pageCollection = [];
 
