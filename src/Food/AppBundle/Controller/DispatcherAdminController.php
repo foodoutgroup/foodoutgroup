884b7d40e182dfc6ca4066dff2e3c7284ca5a527
Aleksas <aseksas@gmail.com>
2017-05-03 16:06:40 +0300

    more error log

--- src/Food/AppBundle/Controller/DispatcherAdminController.php
+++ src/Food/AppBundle/Controller/DispatcherAdminController.php
@@ -468,7 +468,7 @@ class DispatcherAdminController extends Controller
             $logisticsService->assignDriver($driverId, $ordersIds);
         } catch (\Exception $e) {
             // TODO normalus error return ir ispiesimas popupe
-            $this->get('logger')->error('Error happened assigning a driver: ' . $e->getMessage());
+            $this->get('logger')->error('Error happened assigning a driver: ' . $e->getMessage().' '. $e->getFile().' '.$e->getLine()git );
 
             return new Response('Error: error occured');
         }
