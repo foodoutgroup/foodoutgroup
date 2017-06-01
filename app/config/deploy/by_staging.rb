a74c2f1ea2b49a00f87ebb9985cfc02035c24be7
Karolis M <karolis.m@foodout.lt>
2017-06-01 15:04:28 +0300

    deploy scripts

--- app/config/deploy/by_staging.rb
+++ app/config/deploy/by_staging.rb
@@ -1,18 +1,14 @@
-#set :domain,      "192.168.10.113"
-set :domain,      "foodout.lt"
-#set :deploy_to,   "/home/foodout/"
-set :deploy_to,   "/home/dev.foodout.lt/"
+set :domain,      "d.foodout.lt"
+set :deploy_to,   "/srv/vhosts/staging.foodout.by/"
 
 set :scm,         :git
 set :model_manager, "doctrine"
 
-set :user, "dev"
-set :password, "chaebah5bi7quoonuSaegoh3Eigeish"
+set :user, "foodoutby"
+set :password, "uz4sZNe6UD"
 
 set :symfony_env_prod, "prod"
-ssh_options[:keys] = ["/home/foodout/.ssh/id_rsa"]
-
-set :use_composer, true
+ssh_options[:keys] = ["C:\Users\drawgas\.ssh\id_rsa"]
 
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain                         # This may be the same as your `Web` server
@@ -22,5 +18,10 @@ role :db,         domain, :primary => true       # This is where Symfony2 migrat
 #set :skype_topic, "skanu.lt"
 
 # parameters file
-set :parameters_file, "dev.yml.dist"
+set :parameters_file, "by_staging.yml.dist"
+set :kpi_file, "kpi_production.yml.dist"
 set :robots_file, "robots.dev.txt.dist"
+
+after "deploy" do
+    run "~/sync.sh staging.foodout.by"
+end
