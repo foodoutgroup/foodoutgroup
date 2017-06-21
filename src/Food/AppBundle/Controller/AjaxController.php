7c0b4f22ab74a6c8532535c13cdace9f7999b6fb
Aleksas <aseksas@gmail.com>
2017-06-21 10:06:28 +0300

    translations fix

--- src/Food/AppBundle/Controller/AjaxController.php
+++ src/Food/AppBundle/Controller/AjaxController.php
@@ -329,7 +329,7 @@ class AjaxController extends Controller
             }
 
         } else {
-            $rsp['message'] = $t->trans('error.server.problem.').str_replace(" ", ".", strtolower(trim($rsp['message'])));
+            $rsp['message'] = $t->trans('error.server.problem.'.str_replace(" ", ".", strtolower(trim($rsp['message']))));
         }
 
         return $rsp;
