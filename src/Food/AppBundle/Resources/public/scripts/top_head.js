02563fbfa8cb250874255f13568433c9dd517aee
Matas <matas.kozlovas@gmail.com>
2017-08-21 12:10:31 +0300

    old tag manager --

--- src/Food/AppBundle/Resources/public/scripts/top_head.js
+++ src/Food/AppBundle/Resources/public/scripts/top_head.js
@@ -11,20 +11,6 @@
     m.parentNode.insertBefore(a, m)
 })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga', document.createElement('script'));
 
-// google tag manager
-(function (w, d, s, l, i) {
-    w[l] = w[l] || [];
-    w[l].push({
-        'gtm.start':
-            new Date().getTime(), event: 'gtm.js'
-    });
-    var f = d.getElementsByTagName(s)[0],
-        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
-    j.async = true;
-    j.src =
-        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-    f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'dataLayer', 'GTM-PNDJF9');
 
 // facebook pixel
 !function (f, b, e, v, n, t, s) {
