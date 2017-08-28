6d7004848e85bb297b6b0aeef4c89c45700b5739
Matas <matas.kozlovas@gmail.com>
2017-08-28 13:26:13 +0300

    sendigo invoices

--- src/Food/OrderBundle/Service/InvoiceService.php
+++ src/Food/OrderBundle/Service/InvoiceService.php
@@ -332,9 +332,6 @@ class InvoiceService extends ContainerAware
                 ));
 
                 // TODO this is a temp fix for Mailer lite api
-                $ml->removeAttachments()
-                    ->resetVariables()
-                    ->flush();
 
                 $mailTemplate = $this->container->getParameter('mailer_send_invoice');
                 $mailerResponse = $ml->setVariables($variables)
