1af95cab3097103ec453874d01963b55340bdaa3
antanas <antanas@foodout.lt>
2017-09-19 15:10:17 +0300

    callback

--- src/Food/DishesBundle/Entity/BannerLinks.php
+++ src/Food/DishesBundle/Entity/BannerLinks.php
@@ -6,7 +6,10 @@ use Food\AppBundle\Entity\Uploadable;
 use Gedmo\Mapping\Annotation as Gedmo;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Translatable\Translatable;
+use Symfony\Component\Validator\Constraints\Callback;
 use Symfony\Component\Validator\ExecutionContextInterface;
+use Food\AppBundle\Entity\Uploadable;
+
 
 /**
  * Dish option
@@ -14,7 +17,7 @@ use Symfony\Component\Validator\ExecutionContextInterface;
  * @ORM\Table(name="banner_links", indexes={@ORM\Index(name="deleted_at_idx", columns={"deleted_at"})})
  * @ORM\Entity
  * @ORM\Entity(repositoryClass="Food\DishesBundle\Entity\BannerLinksRepository")
- * @Callback(methods={"isFileSizeValid"})
+ * @Callbac(methods={"isFileSizeValid"})
  * @Gedmo\TranslationEntity(class="Food\DishesBundle\Entity\BannerLinksLocalized")
  */
 class BannerLinks extends Uploadable implements Translatable
