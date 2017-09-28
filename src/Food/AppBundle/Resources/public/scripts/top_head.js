9da38922ba721b209d6dae7f8e1a8dc0d65a610c
Matas <matas.kozlovas@gmail.com>
2017-09-28 09:27:59 +0300

    facebook pixel remove

--- src/Food/AppBundle/Resources/public/scripts/top_head.js
+++ src/Food/AppBundle/Resources/public/scripts/top_head.js
@@ -11,27 +11,6 @@
     m.parentNode.insertBefore(a, m)
 })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga', document.createElement('script'));
 
-
-// facebook pixel
-!function (f, b, e, v, n, t, s) {
-    if (f.fbq) return;
-    n = f.fbq = function () {
-        n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-    };
-    if (!f._fbq) f._fbq = n;
-    n.push = n;
-    n.loaded = !0;
-    n.version = '2.0';
-    n.queue = [];
-    t = b.createElement(e);
-    t.async = !0;
-    t.src = v;
-    s = b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t, s)
-}(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
-
-// adform
-
 (function () {
     var s = document.createElement('script');
     s.type = 'text/javascript';
